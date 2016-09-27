059e4147da8e454d04889ecde4fd3893c9fbe224
heather knight and haley swain <heather@>
2016-09-26 19:27:46 -0700

    initial commit, create folders and files for project

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    var question1Input = $("input#question1").val();
+    var question2Input = $("input#question2").val();
+    var question3Input = $("input#question3").val();
+    var question4Input = $("input#question4").val();
+
+    var arr =["question1", "question2", "question3", "question4"];
+  }
+}
